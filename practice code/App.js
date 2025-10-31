352a0778c92f73599f90adb6c01db898cd8f6fca
Akash Odedara <akashodedara7@gmail.com>
2025-10-31 15:36:12 +0530

    chore: initialize package.json for sky_react project with parcel as devDependency

--- practice code/App.js
+++ practice code/App.js
@@ -4,7 +4,7 @@ const parent = React.createElement(
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am h2 tag"),
-  ]), React.createElement("div", { id: "child2" }, [
+  ]), React.createElement("div", { id: "child2 " }, [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am h2 tag"),
   ])]
